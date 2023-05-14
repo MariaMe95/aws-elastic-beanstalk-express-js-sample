b837f833f8a283fbe2e60426426a4b07b0e3ea63
Maria Merino De La Cueva <mmerino@cloudcoachers.com>
2023-05-14 12:39:53 +0200

    change message

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hola Maria!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
